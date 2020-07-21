de0be9c7d09bf0cd44576a58180901f1dce2d6b5
Alexander <eisven@yandex.ru>
2020-07-21 18:29:48 +0300

    Added db

---
+++ db/mongo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000});
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.on('connected', async () => {
+  console.log('Connected to mongoDb');
+});
+
+module.exports = connection;
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Schema = mongoose.Schema;
+const bookSchema = new Schema({
+  title: String,
+  categoryId: Number,
+  coverPath: String,
+  authorsList: [String],
+  annotation: String,
+  isbn: String,
+  pages: String,
+  publisher: String,
+  year: Number,
+  votesCount: Number,
+  rating: Number
+});
+const Book = mongoose.model('Book', bookSchema);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', bodyParser.json(), async (req, res, next) => {
+  
+
+  // limit = 50
+  // skip
+  // page = 1,2,3,4,5
+
+  // page 0
+  // skip 0
+  // limit 50
+
+  // page 1
+  // skip 50
+  // limit 50
+
+  // page 2
+  // skip 100
+  // limit 50
+
+  // page * limit - limit -- if start page is 1
+  // page * limit -- if start page is 0
+  const limit = 5;
+  const page = req.body.page
+  const skip = page * limit;
+  
+  const books = await Book.find({ $or: [ {title: /Двенадцать/i }, { year: 2001 } ] }, null, { skip, limit }).sort({ rating: -1 });
+  res.send(books);
 });
 
 module.exports = router;
