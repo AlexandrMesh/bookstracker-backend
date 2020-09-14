2161a746f0e91559410b08c36e2d9d29395eaa7c
Alexander <eisven@yandex.ru>
2020-09-14 18:25:35 +0300

    Added checkAuth and expand auth method

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
+
+router.post('/checkAuth', async (req, res) => {
+  const { token } = req.body;
+
+  jwt.verify(token, process.env.SECRET_KEY, async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: 'Incorrect token!' });
+    }
+
+    const { userId } = payload;
+
+    try {
+      const { _id, email } = await User.findById(userId);
+      res.send({ profile: { _id, email } });
+    } catch (err) {
+      return res.status(422).send(err.message);
+    }
+  });
+});
 
 router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
@@ -20,23 +40,51 @@ router.post('/signup', async (req, res) => {
 });
 
 router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, googleToken } = req.body;
+
+  if (googleToken) {
+    try {
+      const { data : { email: googleEmail } } = await axios.post('https://oauth2.googleapis.com/tokeninfo', {
+        id_token: googleToken
+      });
+      if (googleEmail !== email) {
+        return res.status(401).send({ error: 'Invalid google email' });
+      }
+      const user = await User.findOne({ email });
+      let userId;
+      let profile;
+      if (!user) {
+        const newUser = new User({ email, password: googleToken });
+        await newUser.save();
+        userId = newUser._id;
+        profile = { _id: newUser._id, email: newUser.email }
+      } else {
+        userId = user._id;
+        profile = { _id: user._id, email: user.email }
+      }
+      const token = jwt.sign({ userId }, process.env.SECRET_KEY);
+      return res.send({ token, profile });
+      } catch (e) {
+        return res.status(401).send({ error: 'Something went wrong' });
+      }
+  }
 
   if (!email || !password) {
-    return res.status(422).send({ error: 'Must provide email and password' });
+    return res.status(401).send({ error: 'Must provide email and password' });
   }
 
   const user = await User.findOne({ email });
   if (!user) {
-    return res.status(422).send({ error: 'Invalid password or email' });
+    return res.status(401).send({ error: 'Invalid password or email' });
   }
 
   try {
     await user.comparePassword(password);
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
-    res.send({ token });
+    profile = { _id: user._id, email: user.email }
+    return res.send({ token, profile });
   } catch (err) {
-    return res.status(422).send({ error: 'Invalid password or email' });
+    return res.status(401).send({ error: 'Invalid password or email' });
   }
 });
 
