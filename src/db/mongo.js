c2015ec0357d0ac10bb645f39ae990a604b0de80
Alexander Meshok <alexandermeshok@coherentsolutions.com>
2024-06-05 10:43:10 +0300

    release/1.4.0

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbName = process.env.NODE_ENV === 'production' ? 'bookdesk' : 'bookdesk_dev';
-const db2 = `mongodb://adminPeshka:${encodeURIComponent('JDASD&#ASDgsdds')}@91.240.254.163/${dbName}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`
+const db2 = `mongodb://adminPeshka:${encodeURIComponent('JDASD&#ASDgsdds')}@82.146.40.13/${dbName}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`
 
 mongoose.connect(db2, {serverSelectionTimeoutMS: 5000});
 const connection = mongoose.connection;
