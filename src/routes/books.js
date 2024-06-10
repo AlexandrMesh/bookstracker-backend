085f96675fe03e5843e7e4380ed558cc58333a8b
Alexander Meshok <alexandermeshok@coherentsolutions.com>
2024-06-10 13:27:02 +0300

    release/1.4.2

--- src/routes/books.js
+++ src/routes/books.js
@@ -68,7 +68,7 @@ router.get('/', getBooksValidator, async (req, res) => {
   } else {
     result = await Book.aggregate([
       { $unionWith: 'custombooks' },
-      { $match : { $and: [{ language }, (categoryPaths || []).length > 0 ? { categoryPath: { $in: categoryPaths } } : {}, exact && title ? { title: { $regex: `^${title}$`, $options: 'i' } } : title ? { $or: [ { title: { $regex: title, $options: 'i' }}, { authorsList: { $regex: title, $options: 'i' } } ] } : {} ] } },
+      { $match : { $and: [{ language }, !title ? { votesCount: { $gt: 100 } } : {}, (categoryPaths || []).length > 0 ? { categoryPath: { $in: categoryPaths } } : {}, exact && title ? { title: { $regex: `^${title}$`, $options: 'i' } } : title ? { $or: [ { title: { $regex: title, $options: 'i' }}, { authorsList: { $regex: title, $options: 'i' } } ] } : {} ] } },
       { $sort : { [sortType]: sortDirection } },
       { $limit : 10000 },
       { $facet: {
